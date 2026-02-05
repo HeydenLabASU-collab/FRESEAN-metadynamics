2a475d0fd5aaf73ce0e5093e9bf8078e402e492d
masauer2 <masauer2@asu.edu>
2026-02-04 22:27:47 -0700

    Automatic gather of gaussian sigma

--- scripts/protocol_FRESEAN+convergedWTMetad+DCCM/06-ModeProj/standard-deviation.py
+++ scripts/protocol_FRESEAN+convergedWTMetad+DCCM/06-ModeProj/standard-deviation.py
@@ -14,13 +14,18 @@ pts=int(sys.argv[2])
 
 # Read in data and store in dataframe
 EIGVEC_D1 = np.zeros((num_modes,pts))
-EIGVEC_D1 = np.loadtxt(f'{projDispFile}', dtype=float, comments=['#'], usecols=[1,2])
+EIGVEC_D1 = np.loadtxt(projDispFile, dtype=float, comments=['#'], usecols=[1,2])
 
 # Convert numpy array to pandas dataframe
 # Convert from Angstroms to nanometers
 EIGVEC_D1_df = pd.DataFrame(EIGVEC_D1, columns=["Mode 7", "Mode 8"])
 
-range_mode7 = np.std(EIGVEC_D1_df["Mode 7"])
-range_mode8 = np.std(EIGVEC_D1_df["Mode 8"])
-print(f"Standard Deviation of Mode 7 Displacement Projection: {range_mode7}")
-print(f"Standard Deviation of Mode 8 Displacement Projection: {range_mode8}")
+range_mode7 = np.std(EIGVEC_D1_df["Mode 7"])/2
+range_mode8 = np.std(EIGVEC_D1_df["Mode 8"])/2
+print(f'Standard Deviation of Mode 7 Displacement Projection: {range_mode7:.9f} nm')
+print(f'Standard Deviation of Mode 8 Displacement Projection: {range_mode8:.9f} nm')
+
+# writes two lines to a text file
+with open("std_dev.txt", "w") as f:
+    f.write(f"{range_mode7:.9f}\n")
+    f.write(f"{range_mode8:.9f}\n")
